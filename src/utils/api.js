29c98a0672ada00945f515cafd1eda182bd61b37
felipemejias <felipe.mejias50@gmail.com>
2022-08-15 17:29:36 -0300

    forgot switch to heroku api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5738"
-  //baseURL: "https://planera-back.herokuapp.com",
+  //baseURL: "http://localhost:5738"
+  baseURL: "https://planera-back.herokuapp.com",
 });
 
 function buildHeader(token){
